ba9f58a9875562deddb2a48d9a2f841f6a0140d2
hanc.han <chihchun.han@gmail.com>
2024-06-28 13:40:04 +0800

    multiple page build demo

--- vite.config.web.js
+++ vite.config.web.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build:{
+    outDir: "dist/web",
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+        sub: path.resolve(__dirname, "pages/sub.html"),
+      }
+    }
+  }
 })
