6c537a58bb2f94f80c451fe152b3dd82e252ffb2
hanc.han <chihchun.han@gmail.com>
2024-06-28 11:59:03 +0800

    create library build & config

---
+++ vite.config.lib.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  build:{
+    lib:{
+      entry: 'src/library/myLibrary.js',
+      name: 'myLibrary',
+      fileName: "myLibrary"
+    }
+  }
+})
